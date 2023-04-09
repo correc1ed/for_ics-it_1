df55fed5c14a4a87a9d727b78f0319eb774a04e2
correc1ed <91633463+correc1ed@users.noreply.github.com>
2023-04-09 14:39:40 +0300

    Change one

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,9 @@
 -- Создать таблицы (на выходе: файл в репозитории CreateStructure.sql в ветке Tables)
 -- 2.1 dbo.SKU (ID identity, Code, Name)
+-- 2.1.1 Ограничение на уникальность поля Code
 
 CREATE TABLE SKU (
 	ID int IDENTITY,
-	Code int,
+	Code int UNIQUE,
 	_Name nvarchar(255),
 	);
